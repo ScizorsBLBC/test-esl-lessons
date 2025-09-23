9ad1896595f14de11c544d2489777135bfb90f56
ScizorsBLBC <scizorsblbc@gmail.com>
2025-09-23 11:28:18 +0900

    updated vite.js file to point to the right location

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Configured for your ESL-Lessons repository.
-  base: '/ESL-Lessons/', 
+  // For a custom domain, the base path should be the root "/".
+  base: '/', 
 })
 
