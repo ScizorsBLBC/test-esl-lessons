9fe0bbac7c06312d0481e9a82f317aec922bfeb0
ScizorsBLBC <scizorsblbc@gmail.com>
2025-09-27 20:34:02 +0900

    added deploy script update and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // You can set this to '/' for Netlify or './' for GitHub Pages
-  base: '/', 
+  // FIX: Change base path to relative ('./') for GitHub Pages compatibility.
+  base: './', 
 })
